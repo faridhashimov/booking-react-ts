5021cd8011708bacbc1ae144c0c5c59e1d3259e9
Farid <fargmov@gmail.com>
2022-09-09 13:29:13 +0300

    add Explore section items fetching functionality with RTK Query

--- client/src/components/Explore/Explore.tsx
+++ client/src/components/Explore/Explore.tsx
@@ -2,11 +2,16 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Slider } from '../../components'
 import { explore } from '../../data'
+import { useGetPopularDestinationsByCountryQuery } from '../../store/fredbookingapi/fredbooking.api'
+import ExploreItemsLoading from '../Skeletons/ExploreItemsLoading'
 import style from './Explore.module.css'
 
 const Explore = () => {
     const [index, setIndex] = useState<number>(0)
     const [country, setCountry] = useState<string>('')
+
+    const { isFetching, isError, data } =
+        useGetPopularDestinationsByCountryQuery(country)
 
     useEffect(() => {
         const getGeoInfo = async () => {
@@ -24,50 +29,63 @@ const Explore = () => {
         getGeoInfo()
     }, [])
 
-    console.log(country)
-
     return (
         <div className={style.container}>
-            <div className={style.mainWrapper}>
-                <h1 className={style.mainTitle}>Explore {country}</h1>
-                <p className={style.mainDesc}>
-                    These popular destinations have a lot to offer
+            {isFetching && <ExploreItemsLoading />}
+            {isError && (
+                <p style={{ color: 'red' }}>
+                    Something went wrong, plese reload the page or try later.
                 </p>
-                <div className={style.wrapper}>
-                    <Slider
-                        data={explore.length}
-                        maxEl={6}
-                        index={index}
-                        setIndex={setIndex}
-                    />
-                    <div className={style.cardsContainer}>
-                        <div
-                            className={style.cardsWrapper}
-                            style={{
-                                transform: `translateX(${-186 * index}px)`,
-                            }}
-                        >
-                            {explore.map(({ id, img, city, info }) => (
-                                <div key={id} className={style.card}>
-                                    <div className={style.imageContainer}>
-                                        <img
-                                            className={style.image}
-                                            src={img}
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div className={style.info}>
-                                        <h3 className={style.title}>{city}</h3>
-                                        <span className={style.desc}>
-                                            {info}
-                                        </span>
-                                    </div>
-                                </div>
-                            ))}
+            )}
+            {data && (
+                <div className={style.mainWrapper}>
+                    <h1 className={style.mainTitle}>Explore {country}</h1>
+                    <p className={style.mainDesc}>
+                        These popular destinations have a lot to offer
+                    </p>
+
+                    <div className={style.wrapper}>
+                        <Slider
+                            data={data ? data.length : 0}
+                            maxEl={6}
+                            index={index}
+                            setIndex={setIndex}
+                        />
+                        <div className={style.cardsContainer}>
+                            <div
+                                className={style.cardsWrapper}
+                                style={{
+                                    transform: `translateX(${-186 * index}px)`,
+                                }}
+                            >
+                                {data?.map(
+                                    ({ _id, cityPhoto, totalProperties }) => (
+                                        <div key={_id} className={style.card}>
+                                            <div
+                                                className={style.imageContainer}
+                                            >
+                                                <img
+                                                    className={style.image}
+                                                    src={cityPhoto}
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className={style.info}>
+                                                <h3 className={style.title}>
+                                                    {_id}
+                                                </h3>
+                                                <span className={style.desc}>
+                                                    {totalProperties} properties
+                                                </span>
+                                            </div>
+                                        </div>
+                                    )
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
